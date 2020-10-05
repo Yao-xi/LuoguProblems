eae5a6796435fec411933ea08fb671dc2f9da095
Yao-xi <664898320@qq.com>
2020-10-05 20:25:12 +0800

    Default Changelist

---
+++ Problems/src/main/java/com/luogu/p5739/Main.java
@@ -0,0 +1,20 @@
+package com.luogu.p5739;
+
+/*
+ * 求阶乘
+ * 挑战：尝试不使用循环语句（for、while）完成这个任务。
+ * */
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(getFactorial(n));
+    }
+    
+    public static int getFactorial(int n) {
+        return n == 1 ? 1 : n * getFactorial(n - 1);
+    }
+}
